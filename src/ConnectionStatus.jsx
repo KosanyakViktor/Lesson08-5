5f233f88f86279e05323badd564e3bc7d25be29d
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-02-04 11:56:46 +0200

    commit_twotwo

--- src/ConnectionStatus.jsx
+++ src/ConnectionStatus.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
-  state = {
-    isOnline: true,
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: false,
+    }
   }
+
+  // state = {
+  //   isOnline: true,
+  // }
 
    componentDidMount() {
     window.addEventListener('online', this.handleOnline);
@@ -15,24 +24,33 @@ class ConnectionStatus extends Component {
     window.removeEventListener('offline', this.handleOffLine);
   }
 
+  // componentDidUpdate() {
+  //   console.log('Test1')
+  // }
+
   handleOnline = () => {
     this.setState({
-      isOnline: true,
+      // isOnline: true,
+      status: 'online'
     });
   }
 
   handleOffLine = () => {
     this.setState({
-      isOnline: false,
+      // isOnline: false,
+      status: 'offline'
     });
   }
 
   render() {
-    return (
-        this.state.isOnline
-        ? <div className="status">online</div>
-        : <div className="status status_offline">offline</div>
-    );
+        // this.state.isOnline
+        // ? <div className="status">online</div>
+        // : <div className="status status_offline">offline</div>
+        
+        const { status } = this.state;
+        const className = `status ${status === 'offline' ? 'stuts_offline' : ''}`
+        return <div className={className}> {status}</div>
+
   };
 }
 
